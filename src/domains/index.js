a351af79e56499a6b79b105231b75922f07c6059
silvia_phungky <silvia@qlue.id>
2021-03-23 21:39:58 +0700

    export not found domain

--- src/domains/index.js
+++ src/domains/index.js
@@ -1,9 +1,11 @@
 import PokemonListDomain from './pokemon-list'
 import PokemonDetailDomain from './pokemon-detail'
 import MyPokemonListDomain from './my-pokemon-list'
+import NotFoundDomain from './not-found'
 
 export {
   PokemonListDomain,
   PokemonDetailDomain,
-  MyPokemonListDomain
+  MyPokemonListDomain,
+  NotFoundDomain
 }
