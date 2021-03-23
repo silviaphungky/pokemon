72579f046feb97b8c87f2090d5fd71fe1faf71c7
silvia_phungky <silvia@qlue.id>
2021-03-23 21:35:15 +0700

    higher modal z-index

--- src/components/modal/styles.js
+++ src/components/modal/styles.js
@@ -13,7 +13,7 @@ export const Modale = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: 10;
+    z-index: 12;
   }
 
   ${({ isOpen }) => isOpen
@@ -41,7 +41,7 @@ export const ModalDialog = styled.div`
   top: -100%;
   left: 0%;
   right: 0%;
-  z-index: 11;
+  z-index: 12;
   box-shadow:0 5px 10px rgba(0,0,0,0.3);
   -webkit-transform: translate(0, -500%);
   -ms-transform: translate(0, -500%);
