d76c746b82f869415bbc0915fa1967b53ea11451
silvia_phungky <silvia@qlue.id>
2021-03-23 21:40:16 +0700

    add notif when success add poke to pokdex

--- src/domains/pokemon-detail/index.js
+++ src/domains/pokemon-detail/index.js
@@ -3,6 +3,9 @@ import { Loading, MenuBar, Text } from 'components'
 import PokemonApi from 'services/pokemon-api'
 import { PokemonDetailDesktop, PokemonDetailFormModal, PokemonDetailMobile } from './components'
 import { PokemonName } from './styles'
+import { useHistory } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
+import { secondary } from 'components/color-mapping'
 
 const PokemonDetailDomain = ({ props }) => {
 
@@ -17,12 +20,14 @@ const PokemonDetailDomain = ({ props }) => {
 
   const [isError, setIsError] = useState('')
 
+  const history = useHistory()
+
   useEffect(() => {
 
     const { pokemonName } = props.match.params
 
     const bodyElt = document.querySelector('body')
-    bodyElt.style.background = 'orange'
+    bodyElt.style.background = secondary
 
     PokemonApi.getPokemonDetail(pokemonName)
       .then((response) => {
@@ -32,15 +37,16 @@ const PokemonDetailDomain = ({ props }) => {
         setIsLoading(false)
       })
       .catch((error) => {
-        alert('something error when fecthing data')
-        console.log(error)
+        if(error.response.status === 404) {
+          history.push('/404')
+        }
       })
 
     const myPokeStorage = localStorage.getItem('myPokeList')
     const initialmyPokeList = myPokeStorage? JSON.parse(myPokeStorage) : []
     setMyPokeList([...initialmyPokeList])
 
-  }, [props.match.params])
+  }, [history, props.match.params])
 
   const handleClickPokeBall = () => {
     if(!isCatching) {
@@ -72,12 +78,14 @@ const PokemonDetailDomain = ({ props }) => {
         const updatedMyPokeList = [...myPokeList, caughtPoke]
         localStorage.setItem('myPokeList', JSON.stringify(updatedMyPokeList))
         setIsOpenModal(false)
+        toast(`${caughtPoke.name} has been added to Pokedex`)
       }
     }
     else if(myPokeList.length === 0 && caughtPoke.name){
       const updatedMyPokeList = [...myPokeList, caughtPoke]
       localStorage.setItem('myPokeList', JSON.stringify(updatedMyPokeList))
       setIsOpenModal(false)
+      toast(`${caughtPoke.name} has been added to Pokedex`)
     }
     else setIsError('Please enter a nickname')
   }
@@ -121,6 +129,17 @@ const PokemonDetailDomain = ({ props }) => {
             isError={ isError }
             setCaughtPoke={ setCaughtPoke }
             handleAddToMyPokeList={ handleAddToMyPokeList }
+          />
+          <ToastContainer 
+            position="top-right"
+            autoClose={ 3000 }
+            hideProgressBar={ true }
+            newestOnTop={ false }
+            closeOnClick
+            rtl={ false }
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
           />
 
           <MenuBar 
