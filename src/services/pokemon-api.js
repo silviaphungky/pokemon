766650ee07f333110be81d8967e880a229190fc6
silvia_phungky <silvia@qlue.id>
2021-03-24 21:27:25 +0700

    rename props

--- src/services/pokemon-api.js
+++ src/services/pokemon-api.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const baseUrl = 'https://pokeapi.co/api/v2'
 
 const PokemonApi = {
-  getPokemonDetail: async (pokemonCode) => {
-    const pokemonDetailUrl  = `${baseUrl}/pokemon/${pokemonCode}`
+  getPokemonDetail: async (pokemonName) => {
+    const pokemonDetailUrl  = `${baseUrl}/pokemon/${pokemonName}`
     const response = await axios.get(pokemonDetailUrl)
     return response
   }
