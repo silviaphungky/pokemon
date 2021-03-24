cf5413dfff2ba352b1dd9a981867aa7b8d22e0da
silvia_phungky <silvia@qlue.id>
2021-03-24 23:25:18 +0700

    clickable card

--- src/domains/my-pokemon-list/components/card/index.js
+++ src/domains/my-pokemon-list/components/card/index.js
@@ -10,6 +10,7 @@ import { PokemonDetailStats } from 'domains/pokemon-detail/components'
 import { Img } from './styles'
 import POKEMON_TYPE_BADGE_COLOR_MAPPING from 'domains/pokemon-detail/components/type-badge/constant'
 import PropTypes from 'proptypes'
+import { Link } from 'react-router-dom'
 
 const propTypes = {
   poke: PropTypes.shape({
@@ -61,64 +62,66 @@ const MyPokemonListCard = ({
           padding   : '1rem 0.5rem' 
         }}
       >
-        <Img 
-          src={ poke.image } 
-          alt={ poke.name }
-        />
-        <Text 
-          bold
-          textAlign='center'
-          textTransform='uppercase'
-          margin='-2rem 0 1rem'
-          marginMobile='-0.75rem 0 0'
-          fontSizeMobile='1rem'
-        >
-          { poke.speciesName }
-        </Text>
-        <Text 
-          bold
-          margin='0.75rem 0 0'
-          marginMobile='0.75rem 0 0'
-        >
+        <Link to={ `/pokemon/${poke.speciesName}` }>
+          <Img 
+            src={ poke.image } 
+            alt={ poke.name }
+          />
+          <Text 
+            bold
+            textAlign='center'
+            textTransform='uppercase'
+            margin='-2rem 0 1rem'
+            marginMobile='-0.75rem 0 0'
+            fontSizeMobile='1rem'
+          >
+            { poke.speciesName }
+          </Text>
+          <Text 
+            bold
+            margin='0.75rem 0 0'
+            marginMobile='0.75rem 0 0'
+          >
           Nickname:
-        </Text>
-        <Text 
-          margin='0.5rem 0 0'
-          marginMobile='0.25rem 0 0'
-        >
-          { poke.name }
-        </Text>
+          </Text>
+          <Text 
+            margin='0.5rem 0 0'
+            marginMobile='0.25rem 0 0'
+          >
+            { poke.name }
+          </Text>
 
-        <Text 
-          bold
-          margin='0.75rem 0 0'
-          marginMobile='0.75rem 0 0'
-        >
+          <Text 
+            bold
+            margin='0.75rem 0 0'
+            marginMobile='0.75rem 0 0'
+          >
           Caught at:
-        </Text>
-        <Text 
-          margin='0.5rem 0 0'
-          marginMobile='0.25rem 0 0'
-        >
-          { dayjs(poke.date).format('DD MMMM YYYY') }
-        </Text>
+          </Text>
+          <Text 
+            margin='0.5rem 0 0'
+            marginMobile='0.25rem 0 0'
+          >
+            { dayjs(poke.date).format('DD MMMM YYYY') }
+          </Text>
 
-        <Text 
-          bold
-          margin='0.75rem 0 0'
-          marginMobile='0.75rem 0 0'
-        >
+          <Text 
+            bold
+            margin='0.75rem 0 0'
+            marginMobile='0.75rem 0 0'
+          >
           Stats:
-        </Text>
-        {
-          poke.stats.map((stat) => (
-            <PokemonDetailStats 
-              stat={ stat }
-              name={ abbreviations(stat.stat.name) } 
-              key={ stat.stat.name }
-            />
-          ))
-        }
+          </Text>
+          {
+            poke.stats.map((stat) => (
+              <PokemonDetailStats 
+                stat={ stat }
+                name={ abbreviations(stat.stat.name) } 
+                key={ stat.stat.name }
+              />
+            ))
+          }
+        </Link>
         <Button 
           aria-label='release'
           onClick={ () => {
