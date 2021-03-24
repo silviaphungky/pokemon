48db5d52eccf4288d1d0c8a04ec25ef3699c7f9f
silvia_phungky <silvia@qlue.id>
2021-03-24 21:26:39 +0700

    remove ../..

--- src/components/layout/styles.js
+++ src/components/layout/styles.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { IS_MOBILE_SCREEN } from '../../utils/media-queries'
+import { IS_MOBILE_SCREEN } from 'utils/media-queries'
 
 export const Wrapper = styled.div`
   margin: 1.5rem 3rem;
